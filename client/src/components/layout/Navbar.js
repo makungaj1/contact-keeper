b132e0374ecfd18d66b27e948c05ae3258a30f3b
jonathan <myemail@bbc.com>
2019-10-13 19:51:33 -0400

    App ready for deploy

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,19 @@
 import React, { Fragment, useContext } from 'react';
 import AuthContext from '../../context/auth/AuthContex';
+import ContactContext from '../../context/contact/contactContext';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Navbar = ({ title, icon }) => {
 	const authContext = useContext(AuthContext);
+	const contactContext = useContext(ContactContext);
 
 	const { isAuthenticated, user, logout } = authContext;
+	const { clearContacts } = contactContext;
 
 	const onLogout = () => {
 		logout();
+		clearContacts();
 	};
 	return (
 		<div className="navbar bg-primary">
--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const auth = require('./routes/auth');
+// const auth = require('./routes/auth');
 const app = express();
+const connectDB = require('./config/db');
+require('./models/production/prod')(app);
+// import path from 'path';
 
-mongoose
-	.connect('mongodb://localhost/contact-keeper', {
-		useUnifiedTopology: true,
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false
-	})
-	.then(() => console.log('Connected to MongoDB....'))
-	.catch((err) => console.error('Could not connect to MongoDB...', err));
+// Connect Database
+connectDB();
+// mongoose
+// 	.connect('mongodb://localhost/contact-keeper', {
+// 		useUnifiedTopology: true,
+// 		useNewUrlParser: true,
+// 		useCreateIndex: true,
+// 		useFindAndModify: false
+// 	})
+// 	.then(() => console.log('Connected to MongoDB....'))
+// 	.catch((err) => console.error('Could not connect to MongoDB...', err));
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.get('/', (req, res) => res.send('Hello world'));
+// app.get('/', (req, res) => res.send('Hello world'));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
